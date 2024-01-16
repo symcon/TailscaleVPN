15b0edc9345c47e6470c1557ee8ed56a67c6282c
Michael Maroszek <maroszek@gmx.net>
2024-01-16 14:38:28 +0100

    add more feature with starting

--- TailscaleVPN/module.php
+++ TailscaleVPN/module.php
@@ -12,6 +12,8 @@ class TailscaleVPN extends IPSModule
     {
         //Never delete this line!
         parent::Create();
+
+        $this->RegisterPropertyString("AuthKey", "");
     }
 
     public function Destroy()
@@ -62,10 +64,36 @@ class TailscaleVPN extends IPSModule
         $this->UpdateFormField("DownloadIndicator", "visible", false);
     }
 
+    public function StartService()
+    {
+        exec($this->getTarget() . "tailscaled > /var/log/symcon/tailscale.log &");
+
+        // Give it some time to connect
+        IPS_Sleep(2500);
+
+        //Reload Form
+        $this->ReloadForm();
+    }
+
+    public function StartTunnel()
+    {
+        exec($this->getTarget() . "tailscale --auth-key=" . $this->RegisterPropertyString("AuthKey"));
+
+        // Give it some time to connect
+        IPS_Sleep(2500);
+
+        //Reload Form
+        $this->ReloadForm();
+    }
+
     public function GetConfigurationForm() {
         $form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
 
         $version = false;
+        $status = false;
+        $serviceRunning = shell_exec("pidof tailscaled");
+        $tunnelRunning = false;
+
         if (file_exists($this->getTarget() . "tailscale")) {
             $result = shell_exec($this->getTarget() . "tailscale --version");
             if ($result) {
@@ -75,6 +103,7 @@ class TailscaleVPN extends IPSModule
             else {
                 $version = "Broken";
             }
+            $status = shell_exec($this->getTarget() . "tailscale status");
         }
 
         if ($version) {
@@ -88,9 +117,28 @@ class TailscaleVPN extends IPSModule
             $form['actions'][2]['caption'] = $this->Translate('Version') . ': ' . $version;
             $form['actions'][2]['visible'] = true;
 
-            $serviceRunning = shell_exec("pidof tailscaled");
-            $form['actions'][3]['caption'] = $this->Translate('Status') . ': ' . ($serviceRunning ? $this->Translate("Service is running!") : $this->Translate("Service is stopped!"));
+            $form['actions'][3]['caption'] = $this->Translate('Service') . ': ' . ($serviceRunning ? $this->Translate("Running!") : $this->Translate("Stopped!"));
             $form['actions'][3]['visible'] = true;
+        }
+
+        if ($status) {
+            $tunnelRunning = !strstr($status, "Tailscale is");
+            if (!$tunnelRunning) {
+                $form['actions'][5]['caption'] = $status;
+            }
+            else {
+                $form['actions'][5]['caption'] = $this->Translate('Service') . ': ' . $this->Translate('Connected!');
+            }
+            $form['actions'][5]['visible'] = true;
+        }
+
+        if ($version && $version !== "Broken") {
+            if (!$serviceRunning) {
+                $form['actions'][4]['visible'] = true;
+            }
+            else if (!$tunnelRunning) {
+                $form['actions'][6]['visible'] = true;
+            }
         }
 
         return json_encode($form);
