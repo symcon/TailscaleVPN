e334c531472edd1d098353d44410a1f3aea1941c
Michael Maroszek <maroszek@gmx.net>
2025-11-24 12:56:08 +0100

    add relevant path for matter and TBRs

--- TailscaleVPN/module.php
+++ TailscaleVPN/module.php
@@ -354,7 +354,18 @@ class TailscaleVPN extends IPSModule
 
             // Enable forwarding inside kernel only if required
             exec("sysctl -w net.ipv4.ip_forward=1");
-            exec("sysctl -w net.ipv6.conf.all.forwarding=1");
+
+            // Allow all RA announcements for Thread/Matter
+            // Set accept_ra = 2 to allow RAs even when forwarding is enabled
+            $dir = '/proc/sys/net/ipv6/conf/';
+            foreach (scandir($dir) as $ifName) {
+                if ($entry === '.' || $entry === '..' || !is_dir($dir . $ifName)) {
+                    continue;
+                }
+                exec("sysctl -w net.ipv6.conf.' . $ifName . '.forwarding=1");
+                exec("sysctl -w net.ipv6.conf.' . $ifName . '.accept_ra=2");
+                exec("sysctl -w net.ipv6.conf.' . $ifName . '.accept_ra_rt_info_max_plen=128");
+            }
         }
 
         if ($authKey) {
